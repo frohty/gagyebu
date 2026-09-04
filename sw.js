55371f881082eefeeb65050764f4a6ee5d83bc0f
frohty <frohty79@gmail.com>
2026-09-05 01:53:35 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gagyebu-v1';
+const CACHE_NAME = 'gagyebu-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
