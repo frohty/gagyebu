98603ad84c1431b96a4d5fc427c52eb50530e0ef
frohty <frohty79@gmail.com>
2026-09-05 01:45:17 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -14,5 +14,13 @@ self.addEventListener('activate', e => {
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, copy));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
